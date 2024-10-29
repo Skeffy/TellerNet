fc80b1866deeba211f0c992a7d8b757aac196860
Noah Stevens <stevens_nas@yahoo.com>
2024-10-29 19:20:59 -0400

    added getters and setters

--- server/src/main/java/io/github/skeffy/tellernet/model/Transaction.java
+++ server/src/main/java/io/github/skeffy/tellernet/model/Transaction.java
@@ -18,4 +18,48 @@ public class Transaction {
         this.amount = amount;
         this.description = description;
     }
+
+    public Transaction() {
+
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
