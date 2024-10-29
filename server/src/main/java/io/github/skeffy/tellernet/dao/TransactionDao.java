00628ad50cfe6b85f6341f715c5a07c4b6921040
Noah Stevens <stevens_nas@yahoo.com>
2024-10-29 19:23:13 -0400

    added methods for getTansactionsByAccount, createTransaction, and getTransactionById

--- server/src/main/java/io/github/skeffy/tellernet/dao/TransactionDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/TransactionDao.java
@@ -1,4 +1,14 @@
 package io.github.skeffy.tellernet.dao;
 
+import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Transaction;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface TransactionDao {
+    List<Transaction> getTransactionsByAccount(Account account);
+    Transaction createTransaction(BigDecimal amount, String description, Account account, Timestamp timestamp);
+    Transaction getTransactionById(int id);
 }
