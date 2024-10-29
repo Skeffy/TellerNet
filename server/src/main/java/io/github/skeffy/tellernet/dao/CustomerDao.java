a049f207fb58dd353a8d0f96b8f8cc27e88978d0
Noah Stevens <stevens_nas@yahoo.com>
2024-10-28 20:36:20 -0400

    Added method signatures for retreiving customer lists

--- server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
@@ -1,4 +1,15 @@
 package io.github.skeffy.tellernet.dao;
 
+import io.github.skeffy.tellernet.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+
 public interface CustomerDao {
+
+    List<Customer> getCustomersByName(String firstName, String lastName);
+    List<Customer> getCustomersByPhone(String phone);
+    List<Customer> getCustomersByAddress(String address);
+    List<Customer> getCustomersByEmail(String email);
+    List<Customer> getCustomersByDob(Date dob);
 }
