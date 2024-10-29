66c8c58d0fa1ae5e291994a35aed1e1510908e6f
Noah Stevens <stevens_nas@yahoo.com>
2024-10-28 20:36:59 -0400

    implement CustomerDao methods

--- server/src/main/java/io/github/skeffy/tellernet/dao/JdbcCustomerDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/JdbcCustomerDao.java
@@ -1,4 +1,33 @@
 package io.github.skeffy.tellernet.dao;
 
-public class JdbcCustomerDao {
+import io.github.skeffy.tellernet.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+
+public class JdbcCustomerDao implements CustomerDao{
+    @Override
+    public List<Customer> getCustomersByName(String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersByPhone(String phone) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersByAddress(String address) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getCustomersByDob(Date dob) {
+        return null;
+    }
 }
