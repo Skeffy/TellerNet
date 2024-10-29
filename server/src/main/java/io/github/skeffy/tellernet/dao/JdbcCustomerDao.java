fb473d53b1a7cf1f34f2397c15e692dccb292869
Noah Stevens <stevens_nas@yahoo.com>
2024-10-29 19:24:17 -0400

    Initial commit

--- server/src/main/java/io/github/skeffy/tellernet/dao/JdbcCustomerDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/JdbcCustomerDao.java
@@ -1,11 +1,18 @@
 package io.github.skeffy.tellernet.dao;
 
 import io.github.skeffy.tellernet.model.Customer;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Date;
 import java.util.List;
 
 public class JdbcCustomerDao implements CustomerDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcCustomerDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
     @Override
     public List<Customer> getCustomersByName(String firstName, String lastName) {
         return null;
@@ -29,5 +36,20 @@ public class JdbcCustomerDao implements CustomerDao{
     @Override
     public List<Customer> getCustomersByDob(Date dob) {
         return null;
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public int updateCustomer(Customer customer) {
+        return 0;
+    }
+
+    @Override
+    public int deleteCustomer(Customer customer) {
+        return 0;
     }
 }
