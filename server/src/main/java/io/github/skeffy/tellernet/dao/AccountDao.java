042c2da3a76229c6102adf732e1cd99c2d664a75
Noah Stevens <stevens_nas@yahoo.com>
2024-10-30 20:12:21 -0400

    condensed getBy methods into getAccountsByCustomer. Added deleteAccount

--- server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
@@ -1,16 +1,15 @@
 package io.github.skeffy.tellernet.dao;
 
 import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Customer;
 
 import java.util.List;
 
 public interface AccountDao {
 
     Account getAccountById(int accountId);
-    List<Account> getAccountsByName(String firstName, String lastName);
-    List<Account> getAccountsByPhone(String phone);
-    List<Account> getAccountsByEmail(String email);
+    List<Account> getAccountsByCustomer(Customer customer);
     Account createAccount(Account account);
     Account updateNickname(Account account, String nickname);
-
+    int deleteAccount(Account account);
 }
