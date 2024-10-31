47f4eb7657ca1287b45b5efeac1f4d0d8ae2d691
Noah Stevens <stevens_nas@yahoo.com>
2024-10-30 22:04:45 -0400

    Added methods for account CRUD operations

--- server/src/main/java/io/github/skeffy/tellernet/controller/AccountController.java
+++ server/src/main/java/io/github/skeffy/tellernet/controller/AccountController.java
@@ -1,4 +1,60 @@
 package io.github.skeffy.tellernet.controller;
 
+import io.github.skeffy.tellernet.dao.AccountDao;
+import io.github.skeffy.tellernet.exception.DaoException;
+import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+    private AccountDao accountDao;
+
+    public AccountController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Account getById(@PathVariable int id) {
+        try {
+            return accountDao.getAccountById(id);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public List<Account> getAccounts(@RequestBody Customer customer) {
+        try {
+            return accountDao.getAccountsByCustomer(customer);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Account createAccount(@RequestBody Account account) {
+        try {
+            return accountDao.createAccount(account);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAccount(@RequestBody Account account) {
+        try {
+            accountDao.deleteAccount(account);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
 }
