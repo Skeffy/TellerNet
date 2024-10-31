0a7728e0f4dbf4938692dbd48e24c1fd7116ef5f
Noah Stevens <stevens_nas@yahoo.com>
2024-10-30 20:08:19 -0400

    added getters and setters

--- server/src/main/java/io/github/skeffy/tellernet/model/Customer.java
+++ server/src/main/java/io/github/skeffy/tellernet/model/Customer.java
@@ -21,4 +21,60 @@ public class Customer {
         this.email = email;
         this.dob = dob;
     }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
 }
