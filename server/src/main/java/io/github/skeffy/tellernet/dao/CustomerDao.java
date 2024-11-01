2127318e740af496905a27e5e1808c07c457318e
Noah Stevens <stevens_nas@yahoo.com>
2024-10-31 21:03:17 -0400

    added getCustomers and getCustomersById. Changed use of Date to LocalDate

--- server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
@@ -2,16 +2,19 @@ package io.github.skeffy.tellernet.dao;
 
 import io.github.skeffy.tellernet.model.Customer;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface CustomerDao {
 
+    List<Customer> getCustomers();
+    Customer getCustomerById(int id);
     List<Customer> getCustomersByName(String firstName, String lastName);
     List<Customer> getCustomersByPhone(String phone);
     List<Customer> getCustomersByAddress(String address);
     List<Customer> getCustomersByEmail(String email);
-    List<Customer> getCustomersByDob(Date dob);
+    List<Customer> getCustomersByDob(LocalDate dob);
     Customer createCustomer(Customer customer);
     int updateCustomer(Customer customer);
     int deleteCustomer(Customer customer);
