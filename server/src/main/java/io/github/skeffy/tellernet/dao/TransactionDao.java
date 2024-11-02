90e503fc6130e5ae248a3e765189defadb5d3e0a
Noah Stevens <stevens_nas@yahoo.com>
2024-11-02 01:39:51 -0400

    createTransaction now uses a transaction object as paramtater. Added deleteTransaction

--- server/src/main/java/io/github/skeffy/tellernet/dao/TransactionDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/TransactionDao.java
@@ -3,12 +3,11 @@ package io.github.skeffy.tellernet.dao;
 import io.github.skeffy.tellernet.model.Account;
 import io.github.skeffy.tellernet.model.Transaction;
 
-import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.util.List;
 
 public interface TransactionDao {
     List<Transaction> getTransactionsByAccount(Account account);
-    Transaction createTransaction(BigDecimal amount, String description, Account account, Timestamp timestamp);
     Transaction getTransactionById(int id);
+    Transaction createTransaction(Transaction transaction);
+    int deleteTransaction(int id);
 }
