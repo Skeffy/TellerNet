1372fd185fc2f8f78387fa5b2282e31d68a37a03
Noah Stevens <stevens_nas@yahoo.com>
2024-11-02 01:48:02 -0400

    added methods for CRUD operations

--- server/src/main/java/io/github/skeffy/tellernet/controller/TransactionController.java
+++ server/src/main/java/io/github/skeffy/tellernet/controller/TransactionController.java
@@ -1,7 +1,60 @@
 package io.github.skeffy.tellernet.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.github.skeffy.tellernet.dao.TransactionDao;
+import io.github.skeffy.tellernet.exception.DaoException;
+import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+
+    private TransactionDao transactionDao;
+
+    public TransactionController(TransactionDao transactionDao) {
+        this.transactionDao = transactionDao;
+    }
+
+    @GetMapping
+    public List<Transaction> getTransactions(@RequestBody Account account) {
+        try {
+            return transactionDao.getTransactionsByAccount(account);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public Transaction getTransaction(@RequestParam int id) {
+        try {
+            return transactionDao.getTransactionById(id);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        try {
+            return transactionDao.createTransaction(transaction);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTransaction(@RequestParam int id) {
+        try {
+            transactionDao.deleteTransaction(id);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
+        }
+    }
 }
