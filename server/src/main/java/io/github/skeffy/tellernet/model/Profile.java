97999d2528f5be4eae4237d1efc6d1e405720570
Noah Stevens <stevens_nas@yahoo.com>
2024-11-06 18:11:37 -0500

    Created Profile class for housing a customer, all of their accounts, and all of the associated transactions

--- server/src/main/java/io/github/skeffy/tellernet/model/Profile.java
+++ server/src/main/java/io/github/skeffy/tellernet/model/Profile.java
@@ -1,2 +1,44 @@
-package io.github.skeffy.tellernet.model;public class Profile {
+package io.github.skeffy.tellernet.model;
+
+import java.util.List;
+
+public class Profile {
+
+    private Customer customer;
+    private List<Account> accounts;
+    private List<List<Transaction>> accountTransactions;
+
+    public Profile(Customer customer, List<Account> accounts, List<List<Transaction>> accountTransactions) {
+        this.customer = customer;
+        this.accounts = accounts;
+        this.accountTransactions = accountTransactions;
+    }
+
+    public Profile() {
+
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<List<Transaction>> getAccountTransactions() {
+        return accountTransactions;
+    }
+
+    public void setAccountTransactions(List<List<Transaction>> accountTransactions) {
+        this.accountTransactions = accountTransactions;
+    }
 }
