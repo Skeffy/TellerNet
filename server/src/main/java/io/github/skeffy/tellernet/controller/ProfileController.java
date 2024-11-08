4ee2b9385ef82c9399697449cffa1c2bc6f4156e
Noah Stevens <stevens_nas@yahoo.com>
2024-11-07 21:55:41 -0500

    Created controller for handling front end requests for profiles

--- server/src/main/java/io/github/skeffy/tellernet/controller/ProfileController.java
+++ server/src/main/java/io/github/skeffy/tellernet/controller/ProfileController.java
@@ -1,2 +1,32 @@
-package io.github.skeffy.tellernet.controller;public class ProfileController {
+package io.github.skeffy.tellernet.controller;
+
+
+import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Customer;
+import io.github.skeffy.tellernet.model.Profile;
+import io.github.skeffy.tellernet.service.ProfileBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+
+    private ProfileBuilder profileBuilder;
+
+    public ProfileController(ProfileBuilder profileBuilder) {
+        this.profileBuilder = profileBuilder;
+    }
+
+    @GetMapping(path = "/customer")
+    public Profile getProfile(@RequestBody Customer customer) {
+        return profileBuilder.createProfile(customer);
+    }
+
+    @GetMapping(path = "/account")
+    public Profile getProfile(@RequestBody Account account) {
+        return profileBuilder.createProfile(account);
+    }
 }
