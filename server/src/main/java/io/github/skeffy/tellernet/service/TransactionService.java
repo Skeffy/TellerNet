c21bfbf434f52e8d105fc922dd25819d468e8021
Noah Stevens <stevens_nas@yahoo.com>
2024-11-08 15:31:57 -0500

    Created transaction service to centralize creating transactions and updating account balance in a single databse transaction

--- server/src/main/java/io/github/skeffy/tellernet/service/TransactionService.java
+++ server/src/main/java/io/github/skeffy/tellernet/service/TransactionService.java
@@ -1,2 +1,32 @@
-package io.github.skeffy.tellernet.service;public class TransactionService {
+package io.github.skeffy.tellernet.service;
+
+import io.github.skeffy.tellernet.dao.AccountDao;
+import io.github.skeffy.tellernet.dao.TransactionDao;
+import io.github.skeffy.tellernet.model.Account;
+import io.github.skeffy.tellernet.model.Transaction;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class TransactionService {
+
+    private AccountDao accountDao;
+    private TransactionDao transactionDao;
+
+
+    public TransactionService(AccountDao accountDao, TransactionDao transactionDao) {
+        this.accountDao = accountDao;
+        this.transactionDao = transactionDao;
+    }
+
+    public Transaction transact(Transaction transaction) {
+        Transaction newTransaction = null;
+        Account account = accountDao.getAccountById(transaction.getAccountId());
+        BigDecimal newBalance = account.getBalance().add(transaction.getAmount());
+        account.setBalance(newBalance);
+        newTransaction = transactionDao.createTransaction(transaction);
+        accountDao.transact(account);
+        return newTransaction;
+    }
 }
