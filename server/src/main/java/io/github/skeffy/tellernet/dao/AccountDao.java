3c4ef2fc61d9db69c0467c43e1dfebba17872822
Noah Stevens <stevens_nas@yahoo.com>
2024-11-08 15:21:46 -0500

    Added method signature for transact method to update acccount balance after a new transaction

--- server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
@@ -9,6 +9,7 @@ public interface AccountDao {
 
     Account getAccountById(int accountId);
     List<Account> getAccountsByCustomer(Customer customer);
+    Account transact(Account account);
     Account createAccount(Account account);
     Account updateNickname(Account account);
     int deleteAccount(Account account);
