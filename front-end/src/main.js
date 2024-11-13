de7973fe278c02fbf29132183541dee28f11f2c9
Noah Stevens <stevens_nas@yahoo.com>
2024-11-12 20:09:44 -0500

    Added vuex store implementation

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,14 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createStore } from './store'
+
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
 
+const store = createStore();
 const app = createApp(App)
 
-app.use(router)
-
+app.use(store);
+app.use(router);
 app.mount('#app')
