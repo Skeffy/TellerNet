b4fb211137a6a6f24f43d1f8748d75518d3906be
Noah Stevens <stevens_nas@yahoo.com>
2024-11-12 20:10:14 -0500

    getCustomers now uses parameters instead of trying to send body with get request

--- front-end/src/services/CustomerService.js
+++ front-end/src/services/CustomerService.js
@@ -3,26 +3,34 @@ import axios from 'axios';
 export default {
 
     getCustomers(customer) {
-        return axios.get('/customers', data = {
-            customer
+        return axios.get('/customers', {
+            params: {
+                customerId: customer.customerId,
+                firstName: customer.firstName,
+                lastName: customer.lastName,
+                phone: customer.phone,
+                address: customer.address,
+                email: customer.email,
+                dob: customer.dob
+            }
         });
     },
 
     createCustomer(customer) {
-        return axios.post('/customers', data = {
-            customer
+        return axios.post('/customers', {
+            data: customer
         });
     },
 
     updateCustomer(customer) {
-        return axios.put('/customers', data = {
-            customer
+        return axios.put('/customers', {
+            data: customer
         });
     },
 
     deleteCustomer(customer) {
-        return axios.delete('/customers', data = {
-            customer
+        return axios.delete('/customers', {
+            data: customer
         });
     }
 }
