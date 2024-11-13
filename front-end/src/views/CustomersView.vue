700e34940967ebf94dc528a1e1ad809ab3987bbf
Noah Stevens <stevens_nas@yahoo.com>
2024-11-12 20:10:32 -0500

    Initial commit

---
+++ front-end/src/views/CustomersView.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <div id="customers">
+
+    </div>
+</template>
+
+<style>
+
+</style>
--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script>
+import Search from '@/components/Search.vue';
 
+export default {
+    components: {
+        Search,
+    }
+}
 </script>
 
 <template>
-    
+    <div id="home">
+        <Search />
+    </div>
 </template>
 
 <style>
