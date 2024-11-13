ed1b8247ec3fd29319846f713183df5263108fae
Noah Stevens <stevens_nas@yahoo.com>
2024-11-12 20:11:01 -0500

    Add router links to accounts and customers routes

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,10 @@
 </script>
 
 <template>
+    <nav>
+        <router-link to="/accounts">Accounts</router-link>
+        <router-link to="/customers">Customers</router-link>
+    </nav>
     <main>
         <router-view />
     </main>
