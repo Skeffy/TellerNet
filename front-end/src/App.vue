8ed7b6ff8798ea44aaf35c28c9361f83f5744b37
Noah Stevens <stevens_nas@yahoo.com>
2024-11-14 21:56:58 -0500

    Added basic grid layout

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,9 @@
 </script>
 
 <template>
+    <div id="heading">
+        <h1>TellerNet</h1>
+    </div>
     <nav>
         <router-link to="/accounts">Accounts</router-link>
         <router-link to="/customers">Customers</router-link>
@@ -12,6 +15,28 @@
     </main>
 </template>
 
-<style scoped>
+<style>
+#app {
+    display: grid;
+    grid-template-columns: 1fr, 4fr, 1fr;
+    grid-template-areas: 
+    "heading heading heading"
+    "nav body hold";
+}
 
+#heading {
+    grid-area: heading;
+    display: flex;
+    justify-content: center;
+}
+
+nav {
+    display: flex;
+    flex-direction: column;
+    grid-area: nav;
+}
+
+main {
+    grid-area: body;
+}
 </style>
