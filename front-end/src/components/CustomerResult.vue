fe75a2c09de978fae8613597df3d67c97c9491ca
Noah Stevens <stevens_nas@yahoo.com>
2024-11-14 21:57:11 -0500

    Initial commit

---
+++ front-end/src/components/CustomerResult.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: ['result']
+}
+</script>
+
+<template>
+    <tr>
+        <td>{{ result.firstName }} {{ result.lastName }}</td>
+        <td>{{ result.phone }}</td>
+        <td>{{ result.email }}</td>
+        <td>{{ result.address }}</td>
+        <td>{{ result.dob }}</td>
+    </tr>
+</template>
+
+<style scoped>
+td {
+    border: 1px solid black;
+    padding: 5px;
+}
+</style>
