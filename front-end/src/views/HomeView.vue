563043690ef63ddc483bcb73635dc5861f09fb9a
Noah Stevens <stevens_nas@yahoo.com>
2024-11-14 21:58:03 -0500

    import ResultList and display flex column

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,19 +1,25 @@
 <script>
 import Search from '@/components/Search.vue';
+import ResultsList from '@/components/ResultsList.vue';
 
 export default {
     components: {
         Search,
+        ResultsList
     }
 }
 </script>
 
 <template>
     <div id="home">
-        <Search />
+        <Search/>
+        <ResultsList v-if="this.$store.state.customers.length > 0" />
     </div>
 </template>
 
 <style>
-
+#home {
+    display: flex;
+    flex-direction: column;
+}
 </style>
