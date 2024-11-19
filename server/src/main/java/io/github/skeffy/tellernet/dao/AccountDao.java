61dd8883b09c12096d2feeeff4830d9a33a3c3b9
Noah Stevens <stevens_nas@yahoo.com>
2024-11-18 21:26:35 -0500

    getAccountsByCustomer now requires customerId

--- server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/AccountDao.java
@@ -1,14 +1,13 @@
 package io.github.skeffy.tellernet.dao;
 
 import io.github.skeffy.tellernet.model.Account;
-import io.github.skeffy.tellernet.model.Customer;
 
 import java.util.List;
 
 public interface AccountDao {
 
     Account getAccountById(int accountId);
-    List<Account> getAccountsByCustomer(Customer customer);
+    List<Account> getAccountsByCustomer(int customerId);
     Account transact(Account account);
     Account createAccount(Account account);
     Account updateNickname(Account account);
