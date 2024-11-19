e9951dce903cee565e425d0bf29a09086a35cbf4
Noah Stevens <stevens_nas@yahoo.com>
2024-11-18 21:27:06 -0500

    Remove unused imports

--- server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
+++ server/src/main/java/io/github/skeffy/tellernet/dao/CustomerDao.java
@@ -4,7 +4,6 @@ import io.github.skeffy.tellernet.model.Customer;
 import io.github.skeffy.tellernet.model.Profile;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 public interface CustomerDao {
