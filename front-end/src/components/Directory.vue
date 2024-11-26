c779b59ccf54236249cc067a86e0b8c8efc6abd8
Noah Stevens <stevens_nas@yahoo.com>
2024-11-26 14:27:59 -0500

    Initial commit

---
+++ front-end/src/components/Directory.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+    <aside id="directory">
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/customers">Customers</router-link>
+            <router-link to="/newcustomer">New Customer</router-link>
+            <router-link to="/newaccount">New Account</router-link>
+        </nav>
+    </aside>
+</template>
+
+<style>
+
+</style>
