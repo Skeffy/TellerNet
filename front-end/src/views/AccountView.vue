3ce0f4d91392806fc1d60c861732cd7b76fd5b8b
Noah Stevens <stevens_nas@yahoo.com>
2024-12-20 23:19:17 -0500

    add TransactionList to template

--- front-end/src/views/AccountView.vue
+++ front-end/src/views/AccountView.vue
@@ -1,9 +1,17 @@
 <script>
+import TransactionList from '@/components/lists/TransactionList.vue';
 
+export default {
+    components: {
+        TransactionList
+    }
+}
 </script>
 
 <template>
-
+<div id="account-view">
+    <TransactionList />
+</div>
 </template>
 
 <style>
