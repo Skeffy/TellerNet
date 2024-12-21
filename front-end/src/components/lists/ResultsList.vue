39e6c8c955a3f681f130adf61817a95e915e2687
Noah Stevens <stevens_nas@yahoo.com>
2024-12-20 23:11:05 -0500

    Move to subfolder

---
+++ front-end/src/components/lists/ResultsList.vue
@@ -0,0 +1,36 @@
+<script>
+import CustomerResult from '@/components/lists/CustomerResult.vue';
+
+export default {
+    components: {
+        CustomerResult
+    },
+}
+</script>
+
+<template>
+    <div id="search-results" class="list">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Date of Birth</th>
+                </tr>
+                <CustomerResult v-for="result in this.$store.state.customers" :result="result"/>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+#search-results {
+    justify-items: center;
+}
+
+th {
+    width: 20%;
+}
+</style>
