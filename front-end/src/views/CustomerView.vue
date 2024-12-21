aefd21efcb0750255fd9ff5f400987561d66f289
Noah Stevens <stevens_nas@yahoo.com>
2024-12-20 23:19:47 -0500

    call api to get accounts and store them

--- front-end/src/views/CustomerView.vue
+++ front-end/src/views/CustomerView.vue
@@ -1,9 +1,25 @@
 <script>
+import AccountService from '@/services/AccountService';
+import AccountList from '@/components/lists/AccountList.vue';
 
+export default {
+
+    components: {
+        AccountList,
+    },
+
+    mounted() {
+        AccountService.getAccounts(this.$route.params.id).then( (response) => {
+            this.$store.commit('SET_ACCOUNTS', response.data);
+        });
+    }
+}
 </script>
 
 <template>
-
+    <div id="customer-view">
+        <AccountList />
+    </div>
 </template>
 
 <style>
