f503bf3d2a4400f38584b6a4fd3535220a299895
Noah Stevens <stevens_nas@yahoo.com>
2024-12-21 23:03:53 -0500

    calls api and adds transactions to store on mount

--- front-end/src/views/AccountView.vue
+++ front-end/src/views/AccountView.vue
@@ -1,9 +1,16 @@
 <script>
+import TransactionService from '@/services/TransactionService';
 import TransactionList from '@/components/lists/TransactionList.vue';
 
 export default {
     components: {
         TransactionList
+    },
+
+    mounted() {
+        TransactionService.getTransactions(this.$route.params.id).then( (response) => {
+            this.$store.commit('SET_TRANSACTIONS', response.data);
+        });
     }
 }
 </script>
