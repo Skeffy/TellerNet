54d76e6db6b6103d4025eb650f47d0ed96977d53
Noah Stevens <stevens_nas@yahoo.com>
2024-12-23 23:34:56 -0500

    home router-link now clears store data

--- front-end/src/components/Directory.vue
+++ front-end/src/components/Directory.vue
@@ -1,11 +1,17 @@
 <script>
-
+export default {
+    methods: {
+        clearStore() {
+            this.$store.commit('CLEAR');
+        }
+    }
+}
 </script>
 
 <template>
     <aside id="directory">
         <nav>
-            <router-link to="/">Home</router-link>
+            <router-link to="/" @click="clearStore">Home</router-link>
             <router-link to="/customers">Customers</router-link>
             <router-link to="/newcustomer">New Customer</router-link>
             <router-link to="/newaccount">New Account</router-link>
