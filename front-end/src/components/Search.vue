6babc13e26ab2c9fcfe8764c2b20e5c47b444bb2
Noah Stevens <stevens_nas@yahoo.com>
2024-12-26 21:57:33 -0500

    refromat form layout

--- front-end/src/components/Search.vue
+++ front-end/src/components/Search.vue
@@ -35,24 +35,31 @@ export default {
             <input type="text" name="lname" id="lname" v-model="customer.lastName">
             <label for="phone">Phone: </label>
             <input type="tel" name="phone" id="phone" v-model="customer.phone">
-            <br>
+            <br><br>
             <label for="address">Address: </label>
             <input type="text" name="address" id="address" v-model="customer.address">
             <label for="email">Email: </label>
             <input type="email" name="email" id="email" v-model="customer.email">
             <label for="dob">DOB: </label>
             <input type="date" name="dob" id="dob" v-model="customer.dob">
-            <input type="button" value="Search" @click="search">
+            <input class="submit" type="button" value="Search" @click="search">
         </form>
     </div>
 </template>
 
 <style>
 #search {
+    display: flex;
+    align-items: center;
+    height: 100px;
     margin-top: 25px;
     margin-bottom: 25px;
     padding-left: 10px;
     padding-right: 10px;
+}
+
+.submit {
+    margin-left: 20px;
 }
 
 label {
