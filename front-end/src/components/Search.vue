644806d754c362532f482c45d604cc77a976a43a
Noah Stevens <stevens_nas@yahoo.com>
2024-12-27 16:19:13 -0500

    reformat form to span 3 lines

--- front-end/src/components/Search.vue
+++ front-end/src/components/Search.vue
@@ -33,11 +33,12 @@ export default {
             <input type="text" name="fname" id="fname" v-model="customer.firstName">
             <label for="lname">Last: </label>
             <input type="text" name="lname" id="lname" v-model="customer.lastName">
+            <br>
             <label for="phone">Phone: </label>
             <input type="tel" name="phone" id="phone" v-model="customer.phone">
-            <br><br>
             <label for="address">Address: </label>
             <input type="text" name="address" id="address" v-model="customer.address">
+            <br>
             <label for="email">Email: </label>
             <input type="email" name="email" id="email" v-model="customer.email">
             <label for="dob">DOB: </label>
